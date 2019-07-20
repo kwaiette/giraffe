2fe1f7ad4755b761036b5ca21699334b0f2f7a36
Wayne Jensen <jensenw@gmail.com>
2019-07-19 18:57:09 -0600

    pipeline path name

--- pipeline/stage/docker-build.sh
+++ pipeline/stage/docker-build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/ec2-user/giraffe/pipeline/stage/
+cd /home/ec2-user/giraffe/pipeline/grfstage/
 sudo /usr/local/bin/docker-compose down
 sudo /usr/local/bin/docker-compose build
 sudo /usr/local/bin/docker-compose up -d
