426911426c178aa43e81be6e049e6e4e9f017799
Wayne Jensen <jensenw@gmail.com>
2019-08-08 17:13:29 -0600

    path fix

--- pipeline/grfprod/docker-build.sh
+++ pipeline/grfprod/docker-build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/ec2-user/giraffe/pipeline/grfprod/
+cd /home/ec2-user/src/giraffe/pipeline/grfprod/
 sudo /usr/local/bin/docker-compose down
 sudo /usr/local/bin/docker-compose build
 sudo /usr/local/bin/docker-compose up -d
